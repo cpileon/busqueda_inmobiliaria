8c7d4cb88a31b8375d48cea0ddcde0192d664c75
Constanza Pino <cpino012@gmail.com>
2023-03-04 23:53:39 -0300

    First commit

---
+++ assets/js/index.js
@@ -0,0 +1,129 @@
+const propiedadesJSON = [
+    {
+      name: "Casa de campo",
+      description: "Un lugar ideal para descansar de la ciudad",
+      src:
+        "https://www.construyehogar.com/wp-content/uploads/2020/02/Dise%C3%B1o-casa-en-ladera.jpg",
+      rooms: 2,
+      m: 170
+    },
+    {
+      name: "Casa de playa",
+      description: "Despierta tus días oyendo el oceano",
+      src:
+        "https://media.chvnoticias.cl/2018/12/casas-en-la-playa-en-yucatan-2712.jpg",
+      rooms: 2,
+      m: 130
+    },
+    {
+      name: "Casa en el centro",
+      description: "Ten cerca de ti todo lo que necesitas",
+      src:
+        "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+      rooms: 1,
+      m: 80
+    },
+    {
+      name: "Casa rodante",
+      description: "Conviertete en un nómada del mundo sin salir de tu casa",
+      src:
+        "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+      rooms: 1,
+      m: 6
+    },
+    {
+      name: "Departamento",
+      description: "Desde las alturas todo se ve mejor",
+      src:
+        "https://www.adondevivir.com/noticias/wp-content/uploads/2016/08/depto-1024x546.jpg",
+      rooms: 3,
+      m: 200
+    },
+    {
+      name: "Mansión",
+      description: "Vive una vida lujosa en la mansión de tus sueños ",
+      src:
+        "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+      rooms: 5,
+      m: 500
+    }
+  ];
+  //Creación de variables
+  
+  const datosPropiedades = document.querySelector(".propiedades");
+  const totalPropiedades = document.querySelector(".py-3 span");
+  const minRooms = document.querySelector("#minCuartos");
+  const minMetros = document.querySelector("#minM");
+  const maxMetros = document.querySelector("#maxM");
+  const formBusqueda= document.querySelector("#busqueda");
+
+
+// Función para generar htm desde un array 
+const generarArray = (array) => {
+  let html = "";
+  if (array.length === 0){
+    totalPropiedades.innerHTML = "¡No hay propiedades con las características que buscas!";
+    datosPropiedades.innerHTML = html;
+    return;
+  }
+  for (let propiedad of array){
+              html += `
+                        <div class="propiedad">
+                          <div class="img" style="background-image: url('${propiedad.src}')"></div>
+                            <section>
+                                <h5>${propiedad.name}</h5>
+                                <div class="d-flex justify-content-between">
+                                    <p>Cuartos: ${propiedad.rooms}</p>
+                                    <p>Metros: ${propiedad.m}</p>
+                                </div>
+                                <p class="my-3">${propiedad.description}</p>
+                                <button class="btn btn-info ">Ver más</button>
+                            </section>
+                        </div>
+                      `
+  }
+
+  datosPropiedades.innerHTML = html;
+  totalPropiedades.innerHTML = array.length;
+  }
+
+  generarArray(propiedadesJSON);
+
+  formBusqueda.addEventListener("reset", () => generarArray(propiedadesJSON));
+  formBusqueda.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    const minimM = +minMetros.value;
+    const maximM = +maxMetros.value;
+    const minimRooms = +minRooms.value;
+    const arrayNuevo = [];
+    let falta = [];
+
+    if (minimM === 0 || maximM === 0 || minimRooms === 0){
+        if(minimM === 0){
+          falta.push(" \n\u2022 Minimo de métros cuadrados");
+        } 
+        if (maximM === 0){
+          falta.push(" \n\u2022 Máximo metros cuadrados");
+        }
+        if (minimRooms === 0){
+          falta.push(" \n\u2022 Mínimo de habitaciones");
+        }
+        console.log(falta);
+        alert(`¡FALTA INFORMACIÓN! \n\Por favor coloque valores válidos en los siguientes campos: ${falta}.`);
+        generarArray(propiedadesJSON);
+
+    } else {
+      console.log(`Mínimo piezas: ${minimRooms}; Minimo metros: ${minimM}; Máximo metros: ${maximM}`)
+      for(let item of propiedadesJSON){
+        if(item.rooms >= minimRooms && item.m >= minimM && item.m <= maximM ){
+          arrayNuevo.push(item)
+        }
+      }
+      console.log(arrayNuevo);
+      generarArray(arrayNuevo);
+    }
+  });
+
+
+
+
